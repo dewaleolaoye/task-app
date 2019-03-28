b43db2fbb1e2641edf07f70230f63395b8e108b3
Engr - six <esix@localhost.localdomain>
2019-03-28 15:57:05 +0100

    insert into database

--- mongodb.js
+++ mongodb.js
@@ -6,5 +6,48 @@ const databaseName = "task-manager";
 
 mongodb.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) return "Unable to connect to the Database";
-  console.log("Database Connected");
+  // console.log("Database Connected");
+  const db = client.db(databaseName);
+  // users collection
+  db.collection("users").insertMany(
+    [
+      {
+        name: "Adewale",
+        age: 27
+      },
+      {
+        name: "Clinton",
+        age: 42
+      }
+    ],
+    (error, result) => {
+      if (error) {
+        return console.log("Unable to insert users");
+      }
+      console.log(result.ops);
+    }
+  );
+  // task collection
+  db.collection("tasks").insertMany(
+    [
+      {
+        description: "Learn React in April",
+        completed: false
+      },
+      {
+        description: "Use piggybank to save",
+        completed: false
+      },
+      {
+        description: "Start writing in April",
+        completed: false
+      }
+    ],
+    (error, result) => {
+      if (error) {
+        return console.log("Unable to insert users description");
+      }
+      console.log(result.ops);
+    }
+  );
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 ## MongoDb
 
 - I had challenges installing mongo to Linux centos 7
+
+- specify the port to see the mongod.log files mongod --port 27018
+- also specify the port for mongo to avoid conflict
