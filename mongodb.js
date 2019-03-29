0089dc834214f7b4fd545533047a4ee736f301b6
Engr - six <esix@localhost.localdomain>
2019-03-29 11:22:38 +0100

    updateOne and updateMany

--- mongodb.js
+++ mongodb.js
@@ -13,49 +13,36 @@ MongoClient.connect(
     if (error) return "Unable to connect to the Database";
     // console.log("Database Connected");
     const db = client.db(databaseName);
-    //     db.collection("users").findOne(
-    //       { _id: new ObjectID("5c9ce0812b880c67154fab18") },
-    //       (error, user) => {
-    //         if (error) {
-    //           return console.log("Unable to find user");
-    //         }
-    //         console.log(user);
+    // update
+    // db.collection("users")
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID("5c9ce59a47e52d1778ffa7bb")
+    //     },
+    //     {
+    //       $inc: {
+    //         age: 5
     //       }
-    //     );
-
-    //     db.collection("users")
-    //       .find({ age: 27 })
-    //       .toArray((error, user) => {
-    //         if (error) {
-    //           return console.log("Unable to find users");
-    //         }
-    //         console.log(user);
-    //       });
-    // // count
-    //     db.collection("users")
-    //       .find({ name: "Adewale" })
-    //       .count((error, count) => {
-    //         console.log(count);
-    //       });
-
-    // findOne
-    db.collection("tasks").findOne(
-      { _id: new ObjectID("5c9ce0812b880c67154fab1c") },
-      (err, task) => {
-        if (err) {
-          return console.log("Unable to find user with id");
-        }
-        console.log(task);
-      }
-    );
-    // find
+    //     }
+    //   )
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(error => {
+    //     console.log("Errors: ", error);
+    //   });
     db.collection("tasks")
-      .find({ completed: false })
-      .toArray((error, task) => {
-        if (error) {
-          return console.log("Unable to find uncompleted task");
-        }
-        console.log(task);
+      .updateMany(
+        {
+          completed: false
+        },
+        { $set: { completed: true } }
+      )
+      .then(result => {
+        console.log(result.modifiedCount);
+      })
+      .catch(error => {
+        console.log(error);
       });
   }
 );
