219fe64c2ba25850ee516d6a7f2d13ef3333f9ed
Engr - six <esix@localhost.localdomain>
2019-03-29 17:11:18 +0100

    model

--- src/mongoose.js
+++ src/mongoose.js
@@ -3,22 +3,52 @@ mongoose.connect("mongodb://127.0.0.1:27018/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true
 });
-
+// mongoose database model
 const User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    required: true
   },
   age: {
-    type: Number
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive Number");
+      }
+    }
   }
 });
 
-const me = new User({
-  name: "Adewale",
-  age: 45
+// const me = new User({
+//   name: "Adewale",
+//   age: 45
+// });
+
+// me.save()
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    required: true
+  },
+  completed: {
+    type: Boolean
+  }
 });
 
-me.save()
+const myTask = new Task({
+  description: "Learn ReactJs Next Month",
+  completed: false
+});
+
+myTask
+  .save()
   .then(result => {
     console.log(result);
   })
